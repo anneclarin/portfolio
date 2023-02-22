a9dae4c59c8702b728f849936e6ceaf0a5b349a1
AnneClarin <amtclarin@gmail.com>
2023-02-22 15:39:08 -0700

    adding media queries

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -10,20 +10,20 @@ export default function Contact() {
             If you would like to chat, send me a message below and I'll be in
             touch! I look forward to hearing from you!</p>
         <form target="_blank" action="https://formsubmit.co/amtclarin@gmail.com" method="POST">
-          <div class="form-group">
-            <div class="form-row">
-              <div class="col">
-                <input type="text" name="name" class="form-control" placeholder="Full Name" required />
+          <div className="form-group">
+            <div className="form-row">
+              <div className="col">
+                <input type="text" name="name" className="form-control" placeholder="Full Name" required />
               </div>
               <div class="col">
-                <input type="email" name="email" class="form-control" placeholder="Email Address" required />
+                <input type="email" name="email" className="form-control" placeholder="Email Address" required />
               </div>
             </div>
           </div>
-          <div class="form-group">
-            <textarea placeholder="Your Message" class="form-control" name="message" rows="10" required></textarea>
+          <div className="form-group">
+            <textarea placeholder="Your Message" className="form-control" name="message" rows="10" required></textarea>
           </div>
-          <button type="submit" class="btn btn-lg btn-dark btn-block">Send!</button>
+          <button type="submit">Send!</button>
         </form>
       </div>
     </section>
