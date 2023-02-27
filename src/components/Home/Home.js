0314d0d2a2ee04e684f6a8c0655eecaf93bfff14
AnneClarin <amtclarin@gmail.com>
2023-02-27 12:36:11 -0700

    padding deletion

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -10,7 +10,7 @@ export default function Home() {
         I am a software developer based in Calgary, AB, Canada. <br/> <br/>
         I love making apps that not only look aesthetically pleasing,
         but can be used to improve your day-to-day life!
-        I'm always in awe when the code takes you from a blank screen to a beautiful,
+        I'm always in awe when code takes you from a blank screen to a beautiful,
         fully-functioning application. <br/> <br/>
         See below for more about me! <br/> <br/>
         <Link activeClass='active' className='link' spy to='#About' smooth>
